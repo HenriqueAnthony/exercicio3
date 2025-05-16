c4885c5ab8c19a25f9a233e308fca2c2e5faa4a8
Henrique <henriqueanthonycs@gmail.com>
2025-05-16 13:20:08 -0300

    quase

--- src/icpstore_backend/main.mo
+++ src/icpstore_backend/main.mo
@@ -1,35 +1,176 @@
 import Array "mo:base/Array";
+import IcpscLedger "canister:icpsc_icrc1_ledger_canister";
+import Principal "mo:base/Principal";
+import Result "mo:base/Result";
+import Error "mo:base/Error";
 
-actor {
-  
-  type Product = {
-    title: Text;
-    description: Text;
-    price: Text;
-    image: Text;    
-  };    
-  
-  public query func getProducts() : async [Product] {    
-      
-      var products : [Product] = []; 
-
-      let p1 : Product = { title = "Motoko para Iniciantes"; description = "Aprenda os fundamentos da linguagem Motoko e como criar DApps na ICP."; price = "50"; image = "../motoko.jpeg"};
-      let p2 : Product = { title = "Tokens ICRC-1 e ICRC-2"; description = "Descubra como criar e gerenciar tokens na Internet Computer."; price = "70"; image = "../tokens.jpeg"};
-      let p3 : Product = { title = "Front-end com React na ICP"; description = "Construa interfaces modernas para DApps usando React na ICP."; price = "20"; image = "../frontend.jpeg"};
-      let p4 : Product = { title = "Chain Fusion na Prática"; description = "Entenda o o que é o projeto Chain Fusion e como utilizar ele na ICP."; price = "100"; image = "../chain_fusion.jpeg" };
-      let p5 : Product = { title = "Dominando HTTPS Outcalls"; description = "Aprenda a realizar chamadas HTTPS para serviços externos a partir da blockchain da ICP."; price = "60"; image = "../http.jpeg" };
-      let p6 : Product = { title = "NFTs na ICP"; description = "Aprenda como criar, mintar e vender NFTs usando a Internet Computer."; price = "100"; image = "../nft.jpeg"};
-
-      products := Array.append(products, [p1]);
-      products := Array.append(products, [p2]);
-      products := Array.append(products, [p3]);
-      products := Array.append(products, [p4]);
-      products := Array.append(products, [p5]);
-      products := Array.append(products, [p6]);
-
-      return products;
+actor Icpstore {
+  //retorna nome
+  public func getTokenName() : async Text {
+    let name = await IcpscLedger.icrc1_name();
+    return name;
+  };
+  // retorna symbol
+  public func getTokenSymbol() : async Text {
+    let symbol = await IcpscLedger.icrc1_symbol();
+    return symbol;
+  };
+  //retorna o supply
+  public func getTokenTotalSupply() : async Nat {
+    let totalSupply = await IcpscLedger.icrc1_total_supply();
+    return totalSupply;
+  };
+  //retorna a taxa de fee do token
+  public func getTokenFee() : async Nat {
+    let fee = await IcpscLedger.icrc1_fee();
+    return fee;
   };
 
+  //retorna o principal ID da identidade minter
+  public func getTokenMintingPrincipal() : async Text {
+    let mintingAccountOpt = await IcpscLedger.icrc1_minting_account();
 
+    switch (mintingAccountOpt) {
+      case (null) { return "Nenhuma conta de mintagem localizada!" };
+      case (?account) {
+        // Converte o principal para texto
+        return Principal.toText(account.owner);
+      };
+    };
+  };
+  // um recorte com todas as informações
+  type TokenInfo = {
+    name : Text; //Nome completo do token (ex: "Internet Computer Protocol")
+    symbol : Text; //Símbolo do token (ex: "ICP")
+    totalSupply : Nat; //Quantidade total de tokens em circulação, representada como Nat (número natural não-negativo)
+    fee : Nat; //Taxa de transferência do token
+    mintingPrincipal : Text; //Identificador do Principal autorizado a emitir novos tokens
+  };
 
+  public func getTokenInfo() : async TokenInfo {
+
+    let name = await getTokenName();
+    let symbol = await getTokenSymbol();
+    let totalSupply = await getTokenTotalSupply();
+    let fee = await getTokenFee();
+    let mintingPrincipal = await getTokenMintingPrincipal();
+
+    let info : TokenInfo = {
+      name = name;
+      symbol = symbol;
+      totalSupply = totalSupply;
+      fee = fee;
+      mintingPrincipal = mintingPrincipal;
+    };
+
+    return info;
+  };
+  //parametros de transferencia
+  type TransferArgs = {
+    amount : Nat; //RECEBE A QUANTIDADE QUE SERA TRANSFERIDA
+    toAccount : IcpscLedger.Account; //RECEBE A CONTA DE DESTINO QUE RECEBERA OS TOKENS
+  };
+  /* transferir da ledger do Canister para outra conta */
+  public shared func transfer(args : TransferArgs) : async Result.Result<IcpscLedger.BlockIndex, Text> {
+
+    let transferArgs : IcpscLedger.TransferArg = {
+      memo = null;
+      amount = args.amount;
+      from_subaccount = null;
+      fee = null;
+      to = args.toAccount;
+      created_at_time = null;
+    };
+
+    try {
+      let transferResult = await IcpscLedger.icrc1_transfer(transferArgs);
+
+      switch (transferResult) {
+        case (#Err(transferError)) {
+          return #err("Não foi possível transferir fundos:
+" # debug_show (transferError));
+        };
+        case (#Ok(blockIndex)) { return #ok blockIndex };
+      };
+    } catch (error : Error) {
+      return #err("Mensagem de rejeição: " # Error.message(error));
+    };
+  };
+  //Obter o saldo de tokens
+  public func getBalance(owner : Principal) : async Nat {
+    let balance = await IcpscLedger.icrc1_balance_of({
+      owner = owner;
+      subaccount = null;
+    });
+    return balance;
+  };
+  //Obter o Principal ID
+  public query func getCanisterPrincipal() : async Text {
+    return Principal.toText(Principal.fromActor(Icpstore));
+}; 
+  //obter o saldo de tokens do canister de backend 
+  public func getCanisterBalance() : async Nat {
+      let owner = Principal.fromActor(Icpstore);
+      let balance = await getBalance(owner);      
+      return balance;
+ };
+
+  type Product = {
+    title : Text;
+    description : Text;
+    price : Text;
+    image : Text;
+  };
+
+  public query func getProducts() : async [Product] {
+
+    var products : [Product] = [];
+
+    let p1 : Product = {
+      title = "Motoko para Iniciantes";
+      description = "Aprenda os fundamentos da linguagem Motoko e como criar DApps na ICP.";
+      price = "50";
+      image = "../motoko.jpeg";
+    };
+    let p2 : Product = {
+      title = "Tokens ICRC-1 e ICRC-2";
+      description = "Descubra como criar e gerenciar tokens na Internet Computer.";
+      price = "70";
+      image = "../tokens.jpeg";
+    };
+    let p3 : Product = {
+      title = "Front-end com React na ICP";
+      description = "Construa interfaces modernas para DApps usando React na ICP.";
+      price = "20";
+      image = "../frontend.jpeg";
+    };
+    let p4 : Product = {
+      title = "Chain Fusion na Prática";
+      description = "Entenda o o que é o projeto Chain Fusion e como utilizar ele na ICP.";
+      price = "100";
+      image = "../chain_fusion.jpeg";
+    };
+    let p5 : Product = {
+      title = "Dominando HTTPS Outcalls";
+      description = "Aprenda a realizar chamadas HTTPS para serviços externos a partir da blockchain da ICP.";
+      price = "60";
+      image = "../http.jpeg";
+    };
+    let p6 : Product = {
+      title = "NFTs na ICP";
+      description = "Aprenda como criar, mintar e vender NFTs usando a Internet Computer.";
+      price = "100";
+      image = "../nft.jpeg";
+    };
+
+    products := Array.append(products, [p1]);
+    products := Array.append(products, [p2]);
+    products := Array.append(products, [p3]);
+    products := Array.append(products, [p4]);
+    products := Array.append(products, [p5]);
+    products := Array.append(products, [p6]);
+
+    return products;
+  };
 };
+
--- src/icpstore_frontend/src/App.jsx
+++ src/icpstore_frontend/src/App.jsx
@@ -1,15 +1,29 @@
-import { useState, useEffect } from 'react';
-import { icpstore_backend } from 'declarations/icpstore_backend';
-import { Principal } from '@dfinity/principal';
-import { idlFactory } from '../../declarations/icpsc_icrc1_ledger_canister/icpsc_icrc1_ledger_canister.did.js';
+import { useState, useEffect } from "react";
+import { icpstore_backend } from "declarations/icpstore_backend";
+import { Principal } from "@dfinity/principal";
+import { idlFactory } from "../../declarations/icpsc_icrc1_ledger_canister/icpsc_icrc1_ledger_canister.did.js";
 
 function App() {
-
+  //constante utilizada para guardar o principal da conta de origem
+  const [from, setFrom] = useState("");
+  //constante utilizada para guardar o principal da conta de destino
+  const [to, setTo] = useState("");
+  //constante utilizada para guardar a quantidade a ser transferida
+  const [amount, setAmount] = useState("");
+  //constante utilizada para guardar o saldo de tokens da conta de origem
+  const [balancesFrom, setBalancesFrom] = useState(0);
+  //constante utilizada para guardar o saldo de tokens da conta de destino
+  const [balancesTo, setBalancesTo] = useState(0);
+  //constante utilizada para exibir mensagens
+  const [message, setMessage] = useState("");
 
   const canisterLedgerId = process.env.CANISTER_ID_ICPSC_ICRC1_LEDGER_CANISTER;
-  
-  const host = process.env.DFX_NETWORK === 'ic' ? 'https://mainnet.dfinity.network' : 'http://localhost:4943';
-  
+
+  const host =
+    process.env.DFX_NETWORK === "ic"
+      ? "https://mainnet.dfinity.network"
+      : "http://localhost:4943";
+
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
@@ -19,11 +33,165 @@ function App() {
     init();
   }, []);
 
+  // função utilizada para obter o saldo de tokens da conta de origem
+  async function getAccountFromBalance(account) {
+    try {
+      if (account != "") {
+        setFrom(account);
+        const result = await icpstore_backend.getBalance(
+          Principal.fromText(account)
+        );
+        setBalancesFrom(parseInt(result));
+        setMessage("");
+      }
+    } catch (error) {
+      console.dir(error);
+      setBalancesFrom(0);
+      setMessage("Ocorreu uma falha ao retornar o saldo da Conta de Origem");
+    }
+  }
+
+  // função utilizada para obter o saldo de tokens da conta de destino
+  async function getAccountToBalance(account) {
+    try {
+      if (account != "") {
+        setTo(account);
+        const result = await icpstore_backend.getBalance(
+          Principal.fromText(account)
+        );
+        setBalancesTo(parseInt(result));
+        setMessage("");
+      }
+    } catch (error) {
+      console.dir(error);
+      setBalancesTo(0);
+      setMessage("Ocorreu uma falha ao retornar o saldo da Conta de Destino");
+    }
+  }
+
+  // Função para conectar à Plug Wallet
+  const conectarPlugWallet = async () => {
+    try {
+      //Confere se a carteira Plug está instalada
+      if (!window.ic?.plug) {
+        console.error("Plug Wallet não instalada!");
+        return;
+      }
+
+      // Conectar à Plug Wallet
+      const connected = await window.ic.plug.requestConnect({
+        whitelist: [canisterLedgerId],
+        host: host, // no caso de mainnet será necessário utilizar o host = 'https://mainnet.dfinity.network'
+      });
+
+      if (connected) {
+        console.log("Conectado com sucesso à Plug Wallet");
+        const principal = await window.ic.plug.agent.getPrincipal();
+
+        //Atualiza o campo Conta Origem com o principal da carteira conectada
+        setFrom(principal.toString());
+        //Atualiza o campo saldo da Conta Origem com o saldo do principal da carteira conectada
+        getAccountFromBalance(principal.toString());
+      }
+    } catch (error) {
+      console.error("Erro ao conectar a Plug Wallet:", error);
+    }
+  };
+
+  /*
+     Função utilizada para transferir da carteira Plug conectada para o principal informado no campo Conta Destino (To).
+     Ao solicitar a transferência a carteira Plug será apresentada na tela e será necessário aprovar a transferencia para
+     que ela seja realizada.
+  */
+  const transfer = async () => {
+    //Confere se a carteira Plug está instalada
+    if (!window.ic?.plug) {
+      console.error("Plug Wallet não instalada!");
+      return;
+    }
+
+    //Obtém o status de conexão da carteira Plug
+    let isConnected = await window.ic.plug.isConnected();
+
+    console.log("isConnected");
+    console.log(isConnected);
+    //Verifica se a carteira Plug está conectada.
+    if (!isConnected) {
+      //Caso não estiver conectada, uma nova conexão será realizada
+      await window.ic.plug.requestConnect({
+        whitelist: [canisterLedgerId],
+        host: host, // no caso de mainnet será necessário utilizar o host = 'https://mainnet.dfinity.network'
+      });
+      isConnected = true;
+    }
+
+    if (isConnected) {
+      // Cria um Actor com as configurações referêntes a ledger do token
+      const actorLedger = await window.ic.plug.createActor({
+        canisterId: canisterLedgerId,
+        interfaceFactory: idlFactory,
+      });
+
+      //Configuração dos parametros para a transferência dos tokens
+      const args = {
+        to: {
+          owner: Principal.fromText(to),
+          subaccount: [],
+        },
+        amount: BigInt(amount),
+        memo: [],
+        fee: [BigInt(10000)], //será consumido 10000 tokens de Fee
+        from_subaccount: [],
+        created_at_time: [],
+      };
+
+      let transferencia = "";
+      try {
+        //esta operação não terá custo de aprovação, será considerado apenas o custo do Fee
+        transferencia = await actorLedger.icrc1_transfer(args);
+      } catch (error) {
+        console.log(error);
+        if (
+          error.message === "Invalid certificate: Signature verification failed"
+        ) {
+          /*
+          O erro 'Invalid certificate: Signature verification failed' é comum ocorrer em ambiente local.
+          Ele está relacionado à verificação de certificados no Internet Computer.
+          Na maior das vezes o erro ocorre devido:
+             Ambiente de desenvolvimento: Em ambientes locais, é necessário configurar o agente para aceitar a root key local
+             e executar a função fetchRootKey(), caso isso não for configurado esta exception poderá ocorrer.
+             Problemas de SSL: Alguns navegadores bloqueiam recursos locais devido a certificados SSL inválidos.
+
+
+          Localmente mesmo ocorrendo esta exception a transferência ocorre normalmente. Utilizando o código
+          deste Dapp na mainnet esta exception não irá ocorrer.
+        */
+
+          transferencia =
+            "A transferência foi concluída com sucesso, mas com algumas ressalvas: " +
+            error.message;
+        } else {
+          transferencia = error.message;
+        }
+      }
+
+      //Limpa o campo quantidade
+      setAmount("");
+
+      //Atualizado o campo de saldo da Conta Origem (From)
+      await getAccountFromBalance(from);
+      //Atualizado o campo de saldo da Conta Destino (To)
+      await getAccountToBalance(to);
+
+      setMessage(transferencia);
+    }
+  };
+
   // 🔽 Função de compra
   const handleBuy = async (price) => {
     try {
       if (!window.ic || !window.ic.plug) {
-        alert('Plug Wallet não está instalada.');
+        alert("Plug Wallet não está instalada.");
         return;
       }
 
@@ -34,28 +202,31 @@ function App() {
       });
 
       if (!connected) {
-        alert('Conexão com Plug Wallet falhou.');
+        alert("Conexão com Plug Wallet falhou.");
         return;
       }
 
       const principal = await window.ic.plug.getPrincipal();
-      console.log('Principal do usuário:', principal.toText());
+      console.log("Principal do usuário:", principal.toText());
 
       // Realiza a transferência
       const transferResult = await window.ic.plug.requestTransfer({
-        to: Principal.fromText(process.env.CANISTER_ID_ICPSTORE_BACKEND).toText(),
-        amount: price * 100_000, // Converter ICPSC para 8 casas decimais (ex: 10 → 1_000_000)
+        to: Principal.fromText(
+          process.env.CANISTER_ID_ICPSTORE_BACKEND
+        ).toText(),
+        amount: BigInt(Math.floor(price * 100_000_000)), // Converter ICPSC para 8 casas decimais (ex: 10 → 1_000_000)
         token: {
           canisterId: canisterLedgerId,
-          standard: 'ICRC1',
+          standard: "ICRC1",
         },
       });
+      
 
-      console.log('Transferência completa:', transferResult);
-      alert('Compra realizada com sucesso!');
+      console.log("Transferência completa:", transferResult);
+      alert("Compra realizada com sucesso!");
     } catch (err) {
-      console.error('Erro ao comprar:', err);
-      alert('Falha na compra. Verifique o console.');
+      console.error("Erro ao comprar:", err);
+      alert("Falha na compra. Verifique o console.");
     }
   };
 
